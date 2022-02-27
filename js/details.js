e289be5386283b32521e80cd8dfccd81010f6a6d
SaraRFerrer <91666428+SaraRFerrer@users.noreply.github.com>
2022-02-27 14:09:30 +0100

    finishing touches

--- js/details.js
+++ js/details.js
@@ -6,7 +6,12 @@ const id = params.get("id");
 console.log(id);
 console.log(params);
 
+
+const cors = "https://noroffcors.herokuapp.com/";
 const drinksUrl = "www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" + id;
+
+const corsApiUrl = cors + drinksUrl;
+
 const detailHtml = document.querySelector(".details");
 const drinks = document.querySelector(".drinks");
 
@@ -14,22 +19,23 @@ function error(message="Error"){
     return `<div class"error">${message}</div>`;
 }
 
-console.log(drinksUrl);
+console.log(corsApiUrl);
 
 async function fetchApi() {
 
     try{
 
-        const response = await fetch(drinksUrl);
+        const response = await fetch(corsApiUrl);
         const getDetails = await response.json();
+        const drinkDetails = getDetails.drinks[0];
        
-        console.log(getDetails);
+        console.log(drinkDetails);
         detailHtml.innerHTML = "";
 
-        detailHtml.innerHTML += `<div class=".details_cards"><h2>${getDetails.strDrink}</h2>
-        <p>Type of glass: ${getDetails.strGlass}</p>
-        <p>Instructioms: ${getDetails.strInstructions}</p>
-        <p>Primary Ingredient: ${getDetails.strIngredient1}</p></div>`;
+        detailHtml.innerHTML += `<div class=".details_cards"><h2>${drinkDetails.strDrink}</h2>
+        <p>Type of glass: ${drinkDetails.strGlass}</p>
+        <p>Instructioms: ${drinkDetails.strInstructions}</p>
+        <p>Primary Ingredient: ${drinkDetails.strIngredient1}</p></div>`;
 
 
 
